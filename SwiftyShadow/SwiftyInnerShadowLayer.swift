4f7ba1d209fa4a50c0648e3cc5de6acacf8b1d61
noblakit01 <noblakit01@gmail.com>
2017-07-11 18:30:34 +0700

    Init path for innerShadowLayer

--- SwiftyShadow/SwiftyInnerShadowLayer.swift
+++ SwiftyShadow/SwiftyInnerShadowLayer.swift
@@ -10,7 +10,13 @@ import UIKit
 
 class SwiftyInnerShadowLayer: CALayer {
 
-    var shadowMode = SwiftyShadowMode.all
+    var shadowModes: [SwiftyShadowMode] = [.left, .right, .top, .bottom]
+    
+    convenience init(mode: [SwiftyShadowMode]) {
+        self.init()
+        shadowModes = mode
+    }
+    
     override init() {
         super.init()
         initShadow()
@@ -32,6 +38,27 @@ class SwiftyInnerShadowLayer: CALayer {
     override func layoutSublayers() {
         super.layoutSublayers()
         
+        let top = shadowModes.contains(.top) ? shadowRadius : 0
+        let bottom = shadowModes.contains(.bottom) ? shadowRadius : 0
+        let left = shadowModes.contains(.left) ? shadowRadius : 0
+        let right = shadowModes.contains(.right) ? shadowRadius : 0
+        let shadowRect = CGRect(x: bounds.origin.x - left,
+                                y: bounds.origin.y - top,
+                                width: bounds.width + left + right,
+                                height: bounds.height + top + bottom)
         
+        let path = CGMutablePath()
+        path.addRect(shadowRect)
+        let bezier: UIBezierPath = {
+            if cornerRadius > 0 {
+                return UIBezierPath(roundedRect: bounds, cornerRadius: cornerRadius)
+            } else {
+                return UIBezierPath(rect: bounds)
+            }
+        }()
+        path.addPath(bezier.cgPath)
+        path.closeSubpath()
+        
+        shadowPath = path
     }
 }
