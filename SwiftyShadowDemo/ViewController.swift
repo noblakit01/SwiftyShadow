c59b376e06eb6f6b2bd743a097ee5d96261bbe30
noblakit01 <noblakit01@gmail.com>
2017-07-20 23:34:50 +0700

    Create SwiftyShadowDemo project

---
+++ SwiftyShadowDemo/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftyShadowDemo
+//
+//  Created by luan on 7/20/17.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var testView: SwiftyInnerShadowView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        testView.shadowLayer.shadowRadius = 4
+        testView.shadowLayer.shadowColor = UIColor.red.cgColor
+        testView.shadowLayer.shadowOpacity = 0.8
+        testView.shadowLayer.shadowOffset = CGSize.zero
+        testView.cornerRadius = 120
+    }
+    
+}
+
