208622e3ccedfc26cbf9a48a8641a55da34b2532
noblakit01 <noblakit01@gmail.com>
2017-07-23 23:02:22 +0700

    Create Shadow view in Demo project

--- SwiftyShadowDemo/ViewController.swift
+++ SwiftyShadowDemo/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var testView: SwiftyInnerShadowView!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var shadowView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,10 +26,17 @@ class ViewController: UIViewController {
         testView.cornerRadius = 120
         
         imageView.layer.cornerRadius = 120
-        imageView.layer.shadowRadius = 2
-        imageView.layer.shadowOpacity = 0.4
-        imageView.layer.shadowColor = UIColor.gray.cgColor
+        imageView.layer.shadowRadius = 20
+        imageView.layer.shadowOpacity = 0.8
+        imageView.layer.shadowColor = UIColor.black.cgColor
         imageView.layer.shadowOffset = CGSize.zero
+        
+        shadowView.layer.cornerRadius = 120
+        shadowView.layer.shadowRadius = 20
+        shadowView.layer.shadowOpacity = 0.8
+        shadowView.layer.shadowColor = UIColor.black.cgColor
+        shadowView.layer.shadowOffset = CGSize.zero
+        
         imageView.generateOuterShadow()
     }
     
