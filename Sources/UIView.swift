9fb20f197b23badd53d8183cf4c784d497606e18
Luan Tran <noblakit01@gmail.com>
2017-10-30 22:39:07 +0700

    Create inner shadow view

--- Sources/UIView.swift
+++ Sources/UIView.swift
@@ -33,6 +33,25 @@ extension UIView {
     }
     
     open func generateInnerShadow() {
+        let view = SwiftyInnerShadowView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.shadowLayer.cornerRadius = layer.cornerRadius
+        view.shadowLayer.shadowRadius = layer.shadowRadius
+        view.shadowLayer.shadowOpacity = layer.shadowOpacity
+        view.shadowLayer.shadowColor = layer.shadowColor
+        view.shadowLayer.shadowOffset = CGSize.zero
+        view.clipsToBounds = false
+        view.backgroundColor = UIColor.white
+        
+        superview?.insertSubview(view, aboveSubview: self)
+        
+        let constraints = [
+            NSLayoutConstraint(item: view, attribute: .left, relatedBy: .equal, toItem: self, attribute: .left, multiplier: 1.0, constant: 0.0),
+            NSLayoutConstraint(item: view, attribute: .right, relatedBy: .equal, toItem: self, attribute: .right, multiplier: 1.0, constant: 0.0),
+            NSLayoutConstraint(item: view, attribute: .top, relatedBy: .equal, toItem: self, attribute: .top, multiplier: 1.0, constant: 0.0),
+            NSLayoutConstraint(item: view, attribute: .bottom, relatedBy: .equal, toItem: self, attribute: .bottom, multiplier: 1.0, constant: 0.0),
+            ]
+        superview?.addConstraints(constraints)
     }
     
 }
